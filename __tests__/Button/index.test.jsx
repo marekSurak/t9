68cbb5dbc7bfe10797f568806c4034f5badfe794
Marek Surak <marek.surak@gmail.com>
2022-08-19 14:34:37 +0200

    test: add test for button

---
+++ __tests__/Button/index.test.jsx
@@ -0,0 +1,10 @@
+import { render, screen, fireEvent } from '@testing-library/react'
+import { Button } from 'components/Button'
+
+test('calls onClick prop when clicked', () => {
+  const handleClick = jest.fn()
+  render(<Button onClick={handleClick}>Click Me!</Button>)
+
+  fireEvent.click(screen.getByText(/click me/i))
+  expect(handleClick).toHaveBeenCalledTimes(1)
+})
