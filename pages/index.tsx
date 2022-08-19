5ebf6efcaae89d9db5e27f2698a4224df8502a47
Marek Surak <marek.surak@gmail.com>
2022-08-19 14:09:01 +0200

    feat: add keyboard button

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,13 @@
 import type { NextPage } from 'next'
 
+import { Button } from '../components/Button'
+
 const Home: NextPage = () => {
-  return <main>T9</main>
+  return (
+    <main>
+      <Button>2</Button>
+    </main>
+  )
 }
 
 export default Home
