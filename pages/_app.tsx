8d1efb16bc8ee2e6fb57eaf4ef6bdf16983b75bc
Marek Surak <marek.surak@gmail.com>
2022-08-19 13:28:53 +0200

    chore: add global styles to app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,10 @@
 import type { AppProps } from 'next/app'
 
+import { globalStyles } from '../styles/global'
+
 function MyApp({ Component, pageProps }: AppProps) {
+  globalStyles()
+
   return <Component {...pageProps} />
 }
 
