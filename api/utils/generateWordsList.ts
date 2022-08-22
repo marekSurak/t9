4303ec9cc6c21b892d6d9a598b9fc2108c0b3f4c
Marek Surak <marek.surak@gmail.com>
2022-08-22 22:17:15 +0200

    feat: connect word list lib

--- api/utils/generateWordsList.ts
+++ api/utils/generateWordsList.ts
@@ -1,12 +1,14 @@
-import { wordsListMock } from 'api/mocks/wordsList'
+import { MAX_WORD_LIST_COUNT } from 'api/constants/wordList'
 
 import { generatePredictions } from './generatePredictions'
 
 // Trying to find appropriate words that starts with prediction
 // 4355 -> hello
-export const generateWordsList = (query: string) => {
+export const generateWordsList = (query: string, wordList: string[]) => {
   const predictions = generatePredictions(query)
-  return wordsListMock.filter((word) =>
+  const wordListMatch = wordList.filter((word) =>
     predictions.find((prediction) => word.startsWith(prediction))
   )
+
+  return wordListMatch.slice(0, MAX_WORD_LIST_COUNT)
 }
--- pages/api/words-list.ts
+++ pages/api/words-list.ts
@@ -1,13 +1,17 @@
+import fs from 'fs'
 import type { NextApiRequest, NextApiResponse } from 'next'
+import wordListPath from 'word-list'
 
 import type { TWordsList } from 'api/types/wordsList'
 import { generateWordsList } from 'api/utils/generateWordsList'
+
+const wordArray = fs.readFileSync(wordListPath, 'utf8').split('\n')
 
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<TWordsList>
 ) {
   const { query } = req.query
-  const wordsList = generateWordsList(query as string)
+  const wordsList = generateWordsList(query as string, wordArray)
   res.status(200).json(wordsList)
 }
