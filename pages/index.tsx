614bbd3e92bc675129d56dcc72deb186a9fd83e2
Marek Surak <marek.surak@gmail.com>
2022-08-22 18:17:35 +0200

    refactor: rename numbers to query

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,27 +10,27 @@ import { PhoneMockup } from 'components/PhoneMockup'
 import { Predictions } from 'components/Predictions'
 
 const Home: NextPage = () => {
-  const [enteredNumbers, setEnteredNumbers] = useState<number[]>([])
-  const { data, isLoading } = useGetPredictionsQuery({ query: enteredNumbers })
+  const [query, setQuery] = useState<number[]>([])
+  const { data, isLoading } = useGetPredictionsQuery({ query })
 
   const handleButtonClick = useCallback(
     (value: number) => {
-      setEnteredNumbers((prevState) => [...prevState, value])
+      setQuery((prevState) => [...prevState, value])
     },
-    [setEnteredNumbers]
+    [setQuery]
   )
 
   const handleDeleteQuery = useCallback(() => {
-    setEnteredNumbers((prevState) => prevState.slice(0, -1))
-  }, [setEnteredNumbers])
+    setQuery((prevState) => prevState.slice(0, -1))
+  }, [setQuery])
 
   return (
     <PhoneMockup>
       <Header />
-      <Display>{enteredNumbers}</Display>
+      <Display>{query}</Display>
       <Predictions predictions={data} isLoading={isLoading} />
       <Keyboard onButtonClick={handleButtonClick} />
-      {enteredNumbers.length ? (
+      {query.length ? (
         <DeleteButton onClick={handleDeleteQuery}>x</DeleteButton>
       ) : null}
     </PhoneMockup>
