78b0604767434fa42c0ce73d7d9b1d5f2524c523
Marek Surak <marek.surak@gmail.com>
2022-08-22 11:32:00 +0200

    test: update snapshots

--- components/Predictions/__tests__/index.test.jsx
+++ components/Predictions/__tests__/index.test.jsx
@@ -3,11 +3,6 @@ import { Predictions } from 'components/Predictions'
 import { predictionsMock } from 'api/mocks/predictions'
 
 describe('Predictions', () => {
-  test('Renders null if no props provided ', () => {
-    const elm = render(<Predictions />)
-    expect(elm.container.innerHTML).toHaveLength(0)
-  })
-
   test('Renders correctly all items', () => {
     const { getAllByRole } = render(
       <Predictions predictions={predictionsMock} />
