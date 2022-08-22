79fd6b5ded000fcb0deb491f2aeb546804007377
Marek Surak <marek.surak@gmail.com>
2022-08-22 14:43:57 +0200

    test: add snapshot

--- components/PhoneMockup/__tests__/snapshot.js
+++ components/PhoneMockup/__tests__/snapshot.js
@@ -1,11 +1,13 @@
 import { render } from '@testing-library/react'
 import { PhoneMockup } from 'components/PhoneMockup'
 import { Keyboard } from 'components/Keyboard'
+import { DeleteButton } from 'components/DeleteButton'
 
 it('Renders Phone frame unchanged', () => {
   const { container } = render(
     <PhoneMockup>
       <Keyboard />
+      <DeleteButton onClick={() => {}}>x</DeleteButton>
     </PhoneMockup>
   )
   expect(container).toMatchSnapshot()
