0eb9e341e39724f18e0341be59c1584ff686914b
Marek Surak <marek.surak@gmail.com>
2022-08-24 17:31:54 +0200

    refactor: use word list mock

--- pages/api/words-list.ts
+++ pages/api/words-list.ts
@@ -1,7 +1,6 @@
-import fs from 'fs'
 import type { NextApiRequest, NextApiResponse } from 'next'
-import wordListPath from 'word-list'
 
+import { wordsListMock } from 'api/mocks/wordsList'
 import type { TWordsList } from 'api/types/wordsList'
 import { generateWordsList } from 'api/utils/generateWordsList'
 
@@ -10,7 +9,6 @@ export default function handler(
   res: NextApiResponse<TWordsList>
 ) {
   const { query } = req.query
-  const wordArray = fs.readFileSync(wordListPath, 'utf8').split('\n')
-  const wordsList = generateWordsList(query as string, wordArray)
+  const wordsList = generateWordsList(query as string, wordsListMock)
   res.status(200).json(wordsList)
 }
