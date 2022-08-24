223c0bb7f20411126f67324cac7eb73fa3b6d23a
Marek Surak <marek.surak@gmail.com>
2022-08-24 13:02:08 +0200

    feat: move read file inside req handler

--- pages/api/words-list.ts
+++ pages/api/words-list.ts
@@ -1,18 +1,16 @@
-// import fs from 'fs'
+import fs from 'fs'
 import type { NextApiRequest, NextApiResponse } from 'next'
-// import wordListPath from 'word-list'
+import wordListPath from 'word-list'
 
-import { wordsListMock } from 'api/mocks/wordsList'
 import type { TWordsList } from 'api/types/wordsList'
 import { generateWordsList } from 'api/utils/generateWordsList'
-
-// const wordArray = fs.readFileSync(wordListPath, 'utf8').split('\n')
 
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<TWordsList>
 ) {
   const { query } = req.query
-  const wordsList = generateWordsList(query as string, wordsListMock)
+  const wordArray = fs.readFileSync(wordListPath, 'utf8').split('\n')
+  const wordsList = generateWordsList(query as string, wordArray)
   res.status(200).json(wordsList)
 }
